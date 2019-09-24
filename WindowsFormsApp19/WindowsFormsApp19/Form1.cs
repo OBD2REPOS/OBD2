bb7e8aa608a00bf64291328d4dd926cfb19ef158
TheDutchPotato <griffin02@hotmail.nl>
2019-09-24 15:30:16 +0200

    yeet

--- WindowsFormsApp19/WindowsFormsApp19/Form1.cs
+++ WindowsFormsApp19/WindowsFormsApp19/Form1.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApp19
         {
             InitializeComponent();
         }
-
+        //yeet
         public void createSerialPort()
         {
             //Port name can be identified by checking the ports
